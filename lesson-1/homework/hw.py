5d503968ec623f664e4fcadd36b8929d678392b6
TalatBQ <87007100+TalatBQ@users.noreply.github.com>
2025-10-31 20:50:40 +0500

    Update hw.py

--- lesson-1/homework/hw.py
+++ lesson-1/homework/hw.py
@@ -1,1 +1,38 @@
+#1 Given a side of square. Find its perimeter and area.
+
+a=7
+P=4*a
+S=a*a
+
+print('Perimetr:', P)
+print('Area:', S)
+
+#2 Given diameter of circle. Find its length.
+
+d=5
+L=d*3.14
+
+print('Length:', L)
+
+#3 Given two numbers a and b. Find their mean.
+
+a=18
+b=23
+M=(a+b)/2
+
+print('Mean:', M)
+
+#4 Given two numbers a and b. Find their sum, product and square of each number.
+
+a=11
+b=9
+c=a+b
+p=a*b
+s1=a*a
+s2=b*b
+
+print('Sum:', c)
+print('Product:', p)
+print('Square1:', s1)
+print('Square2:', s2)
 
